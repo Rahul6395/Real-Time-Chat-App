4c8fcf850a2546659a7cfb690efb8cf99907accb
Rahul6395 <102130290+Rahul6395@users.noreply.github.com>
2025-10-19 23:59:52 +0530

    all code first commit

---
+++ server/routes/ContactRoutes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { getContactsForDMList, searchContacts,getAllContacts } from "../controllers/ContactsController.js";
+import { verifyToken } from "../middlewares/AuthMiddleware.js";
+
+const contactRoutes = Router();
+
+contactRoutes.post("/search",verifyToken,searchContacts);
+contactRoutes.get("/get-contacts-for-dm",verifyToken,getContactsForDMList);
+contactRoutes.get('get-all-contacts',verifyToken,getAllContacts);
+
+export default contactRoutes
